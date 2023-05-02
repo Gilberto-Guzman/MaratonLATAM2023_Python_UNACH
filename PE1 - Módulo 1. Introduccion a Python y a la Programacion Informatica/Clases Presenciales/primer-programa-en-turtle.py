f04da31c8145a9d89a9b88727ca12bdf5c753cc5
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-05-02 12:31:28 -0600

    Se actualizo el contenido

---
+++ PE1 - Módulo 1. Introduccion a Python y a la Programacion Informatica/Clases Presenciales/primer-programa-en-turtle.py
@@ -0,0 +1,24 @@
+import turtle
+
+t = turtle.Pen()
+t.penup()
+t.goto(0, 0)
+t.pendown()
+t.goto(200, 200)
+turtle.done()
+
+
+# t = turtle.Pen()
+# t.towards(200)
+# t.left(180)
+# t.towards(200)
+
+# t = turtle.Pen()
+# t.goto(0, 0)
+# t.goto(200, 200)
+
+
+# t = turtle.Pen()
+# t.setpos(0, 0)
+# t.setheading(t.towards(50, 0))
+# t.forward(50)
